29573a2fe22f9003b4737bc2cf65a4e972f8ec85
andre azu <andreazuaa@gmail.com>
2021-09-28 15:49:56 +0300

    add routes

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,10 @@
 # file in charge of holding authentications
+# File in charge of holding routes 
+from  flask import Blueprint,render_template
+
+auth = Blueprint('auth',__name__)
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html" ,text="Hello new user :)") 
