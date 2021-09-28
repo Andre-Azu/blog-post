93197ce47ad176b04e33c2351e33d74115bb3aa3
andre azu <andreazuaa@gmail.com>
2021-09-28 15:34:56 +0300

    add uittesting

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 # File in charge of holding the models.
 from . import db
+from sqlalchemy.sql import func #enables SQL to handle the date column.
+from flask_login import UserMixin #gives users ability to log in
+
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    
     id=db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String(150),unique=True)
     # Unique true ensures no two users can have the same emails
@@ -14,3 +19,5 @@ class Comment(db.Model):
     date=db.Column(db.DateTime(timezone=True),default=func.now())
     # Relationship between the user and comment
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+
