602a09c2ade5f7516e0f25298aea4417933962d3
andre azu <andreazuaa@gmail.com>
2021-09-28 16:23:50 +0300

    add comlete signing up

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,49 @@
 # file in charge of holding authentications
 # File in charge of holding routes 
-from  flask import Blueprint,render_template
+from  flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from . import db
+from werkzeug.security import check_password_hash,generate_password_hash
+from flask_login import login_required,login_user,logout_user,current_user
 
 auth = Blueprint('auth',__name__)
 
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET','POST'])
 def sign_up():
-    return render_template("sign_up.html" ,text="Hello new user :)") 
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+
+        # To prevent a user to sign up with the same email,
+        user=User.query.filter_by(email=email).first()
+        if user:
+            flash("email already exists", category='error')
+        # add validations/flash messages 
+
+        elif len("email")<2:
+            flash("email must be greater than two characters",category='Success')
+        elif len('first_name')<3:
+            flash("firstname too short",category='error')
+        elif len('password')<4:
+            flash('password too short',category='error')
+        elif password1 != password2:
+            flash("passwords dont match", category="error")
+        else:
+            new_user=User(email=email,password=generate_password_hash(password1, method='sha256'),first_name=first_name)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            #add user to database
+            flash('account created',category='success')
+            #to redirect the user to the homepage of the website, 
+            return redirect(url_for('views.home'))
+
+
+    return render_template("sign_up.html" ,text="Hello new user :)",user=current_user)
+
 
 @auth.route('/login')
 def login():
--- app/models.py
+++ app/models.py
@@ -1,23 +1,27 @@
-# File in charge of holding the models.
+# where the databases will be created
 from . import db
-from sqlalchemy.sql import func #enables SQL to handle the date column.
-from flask_login import UserMixin #gives users ability to log in
+
+#gives users ability to log in
+from flask_login import UserMixin
+
+#allows for sql achemy to take care of the date thing 
+from sqlalchemy.sql import func
+
+# db.model ensures that a blueprint is being followed
+class Comment(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    data=db.Column(db.String(10000))
+    date=db.Column(db.DateTime(timezone=True),default=func.now())
+    #How to link the notes to the users, therefore a relationship has to be created via a forein key
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    
     id=db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String(150),unique=True)
     # Unique true ensures no two users can have the same emails
     password=db.Column(db.String(150))
     first_name=db.Column(db.String(150))
-    
-class Comment(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    data=db.Column(db.String(10000))
-    date=db.Column(db.DateTime(timezone=True),default=func.now())
-    # Relationship between the user and comment
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
-
-
+    #linking the user to the notes themseselves 
+    notes=db.relationship('Comment')
