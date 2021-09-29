a50c4aa557455d25b0deba1d37036b9612f37154
andre azu <andreazuaa@gmail.com>
2021-09-29 12:11:09 +0300

    add config.py file

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config:
+    pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    
+config_options = {
+'production':ProdConfig
