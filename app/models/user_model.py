01a9f386a36970000ef9e5bf7a288601bd06cb00
andre azu <andreazuaa@gmail.com>
2021-09-29 10:51:54 +0300

    add user model unittest

---
+++ app/models/user_model.py
@@ -0,0 +1,11 @@
+class Quote:
+    '''
+    Quote class to define Quote Objects
+    '''
+
+    def __init__(self,id,author,quote,permalink):
+        self.id =id
+        self.author = author
+        self.quote = quote
+        self.permalink = permalink
+       
